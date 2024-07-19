8a320dfdcf7401305932f3d7f4258cb80016aaf6
LeonID <lmatskevich79@gmail.com>
2024-07-20 00:29:38 +0300

    feat: add component Filters

--- Rick-end-Morty/src/App.jsx
+++ Rick-end-Morty/src/App.jsx
@@ -1,10 +1,29 @@
 import './App.scss';
+import Filters from './components/Filters/Filters';
 
 function App() {
   return (
     <>
       <div>
-        <h1 className='h1'>Rick & Morty</h1>
+        <div className='container'>
+          <div className='title'>
+            <h1 className='h1'>Rick & Morty</h1>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <section className='section'>
+          <div className='filter'>
+            <Filters />
+          </div>
+        </section>
+        <section className='section'>
+          <div className='cards'>
+            <div className='card'>Card</div>
+            <div className='card'>Card</div>
+            <div className='card'>Card</div>
+          </div>
+        </section>
       </div>
     </>
   );
